69a276e9cca41cd9536c7963408de17d0c644a86
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-10-19 17:50:07 -0300

    chore: add update user validation

--- src/validations/UserValidations.ts
+++ src/validations/UserValidations.ts
@@ -70,5 +70,35 @@ export class UserValidations {
 
         return authenticateUserSchema.safeParse(userData);
     }
+
+    update(userData: any) {
+        const updateUserSchema = zod.object(
+            {
+                name: zod
+                    .string(
+                        {
+                            required_error: "The name is required",
+                            invalid_type_error: "The name must be a string",
+                            description: "The user name"
+                        }
+                    )
+                    .min(3, "The name must have a minimum of 3 characters")
+                    .max(255, "The name must have a maximum of 255 characters"),
+
+                email: zod
+                    .string(
+                        {
+                            required_error: "The email is required",
+                            invalid_type_error: "The email must be a string",
+                            description: "The user email"
+                        }
+                    )
+                    .email("The email must be in a valid format")
+                    .max(255, "The email must have a maximum of 255 characters")
+            }
+        );
+
+        return updateUserSchema.safeParse(userData);
+    }
 }
 /* eslint-enable camelcase */
