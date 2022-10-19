49691890fd5f8f43f36d67019853954ca9d18966
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-10-19 16:28:28 -0300

    refactor: change port usage method at server start file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { app } from "./server";
 
+const port = 3000;
+
 app.listen(
-    3000,
-    () => console.log("Server is running on port 3000")
+    port,
+    () => console.log(`Server running on port ${port}`)
 );
