c4c0b0168dce022fe91c496695413384603571e7
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-10-24 12:12:26 -0300

    feat: update series

--- src/routes/series.routes.ts
+++ src/routes/series.routes.ts
@@ -66,7 +66,7 @@ seriesRoutes.post(
             );
 
             if (doesAllTagsExist.some(tag => !tag))
-                return response.status(400).json(
+                return response.status(404).json(
                     new NotFoundError("Tag not found")
                 );
 
@@ -81,9 +81,9 @@ seriesRoutes.post(
                             createMany: {
                                 skipDuplicates: false,
                                 data: tags.map(
-                                    tag => (
+                                    tagId => (
                                         {
-                                            tagId: tag
+                                            tagId
                                         }
                                     )
                                 )
@@ -242,6 +242,141 @@ seriesRoutes.get(
             );
 
             return response.status(200).json(paginatedSeries);
+        } catch (error) {
+            return handleControllerError(error, response);
+        }
+    }
+);
+
+seriesRoutes.put(
+    "/update/:id",
+    ensureAuthentication,
+    ensureModeration,
+    async (request, response) => {
+        try {
+            const authenticationData = request.user;
+            if (!authenticationData) {
+                /* eslint-disable-next-line no-console */
+                console.error("Expected user authentication at update series route");
+                return response.status(500).json(
+                    new InternalServerError()
+                );
+            }
+
+            const rawSeriesId = Number(request.params.id);
+            const rawSeriesData = request.body;
+            rawSeriesData.id = rawSeriesId;
+
+            const validation = seriesValidations.update(rawSeriesData);
+            if (!validation.success)
+                return response.status(400).json(
+                    new ValidationError(handleZodError(validation.error))
+                );
+
+            const {
+                id: seriesId,
+                mainName,
+                mainNameLanguage,
+                alternativeName,
+                description,
+                tags
+            } = validation.data;
+
+            const [
+                doesSeriesExist,
+                doesAllTagsExist
+            ] = await Promise.all(
+                [
+                    prisma.series.findFirst(
+                        {
+                            select: {
+                                id: true
+                            },
+
+                            where: {
+                                id: seriesId,
+                                deletedAt: null
+                            }
+                        }
+                    ),
+
+                    Promise.all(
+                        tags.map(
+                            tagId => prisma.tag.findFirst(
+                                {
+                                    select: {
+                                        id: true
+                                    },
+
+                                    where: {
+                                        id: tagId,
+                                        deletedAt: null
+                                    }
+                                }
+                            )
+                        )
+                    )
+                ]
+            );
+
+            if (!doesSeriesExist)
+                return response.status(404).json(
+                    new NotFoundError("Series not found")
+                );
+
+            if (doesAllTagsExist.some(tag => !tag))
+                return response.status(404).json(
+                    new NotFoundError("Tag not found")
+                );
+
+            const [
+                _deletedSeriesTags,
+                series
+            ] = await prisma.$transaction(
+                [
+                    prisma.seriesTags.updateMany(
+                        {
+                            data: {
+                                deletedAt: new Date()
+                            },
+
+                            where: {
+                                seriesId,
+                                deletedAt: null
+                            }
+                        }
+                    ),
+
+                    prisma.series.update(
+                        {
+                            data: {
+                                mainName,
+                                mainNameLanguage,
+                                alternativeName,
+                                description,
+                                seriesTags: {
+                                    createMany: {
+                                        skipDuplicates: false,
+                                        data: tags.map(
+                                            tagId => (
+                                                {
+                                                    tagId
+                                                }
+                                            )
+                                        )
+                                    }
+                                }
+                            },
+
+                            where: {
+                                id: seriesId
+                            }
+                        }
+                    )
+                ]
+            );
+
+            return response.status(200).json(series);
         } catch (error) {
             return handleControllerError(error, response);
         }
