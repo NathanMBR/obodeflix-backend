e978c92305e8f9e85d44f955e655fedf7478904c
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-10-24 11:49:45 -0300

    chore: add update series validation

--- src/validations/SeriesValidations.ts
+++ src/validations/SeriesValidations.ts
@@ -136,5 +136,83 @@ export class SeriesValidations {
             search
         };
     }
+
+    update(seriesData: any) {
+        const updateSeriesSchema = zod.object(
+            {
+                id: zod
+                    .number(
+                        {
+                            required_error: "The series ID is required",
+                            invalid_type_error: "The series ID must be a number",
+                            description: "The series ID"
+                        }
+                    )
+                    .int("The series ID must be an integer")
+                    .positive("The series ID must be a positive number"),
+
+                mainName: zod
+                    .string(
+                        {
+                            invalid_type_error: "The main name must be a string",
+                            description: "The series main name"
+                        }
+                    )
+                    .min(1, "The main name must have a minimum of 1 character")
+                    .max(255, "The main name must have a maximum of 255 characters"),
+
+                alternativeName: zod
+                    .string(
+                        {
+                            invalid_type_error: "The alternative name must be a string",
+                            description: "The series alternative name"
+                        }
+                    )
+                    .min(1, "The alternative name must have a minimum of 1 character")
+                    .max(255, "The alternative name must have a maximum of 255 characters")
+                    .optional(),
+
+                mainNameLanguage: zod
+                    .enum(
+                        [
+                            "ENGLISH",
+                            "JAPANESE"
+                        ],
+
+                        {
+                            invalid_type_error: "The main name language must be ENGLISH or JAPANESE",
+                            description: "The series main name language"
+                        }
+                    ),
+
+                description: zod
+                    .string(
+                        {
+                            invalid_type_error: "The description must be a string",
+                            description: "The series description"
+                        }
+                    )
+                    .optional(),
+
+                tags: zod
+                    .array(
+                        zod.number(
+                            {
+                                invalid_type_error: "The tag must be a number",
+                                description: "The series tag"
+                            }
+                        ),
+
+                        {
+                            invalid_type_error: "The series tags must be a number array",
+                            description: "The series tags"
+                        }
+                    )
+                    .transform(removeRepeatedElements)
+            }
+        );
+
+        return updateSeriesSchema.safeParse(seriesData);
+    }
 }
 /* eslint-enable camelcase */
