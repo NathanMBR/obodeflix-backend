7178701ba53236e4e5ff210a2cdf534a7c25acf4
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-10-27 16:57:49 -0300

    chore: add episode validations

---
+++ src/validations/EpisodeValidations.ts
@@ -0,0 +1,221 @@
+import { z as zod } from "zod";
+
+import { getPaginationParameters } from "@/helpers";
+
+/* eslint-disable camelcase */
+export class EpisodeValidations {
+    create(episodeData: any) {
+        const createEpisodeSchema = zod.object(
+            {
+                name: zod
+                    .string(
+                        {
+                            required_error: "The episode name is required",
+                            invalid_type_error: "The episode name must be a string",
+                            description: "The episode name"
+                        }
+                    )
+                    .min(1, "The episode name must have a minimum of 1 character")
+                    .max(255, "The episode name must have a maximum of 255 characters"),
+
+                seasonId: zod
+                    .number(
+                        {
+                            required_error: "The episode season ID is required",
+                            invalid_type_error: "The episode season ID must be a number",
+                            description: "The episode season ID"
+                        }
+                    )
+                    .int("The episode season ID must be an integer")
+                    .positive("The episode season ID must be a positive number"),
+
+                duration: zod
+                    .number(
+                        {
+                            required_error: "The episode duration is required",
+                            invalid_type_error: "The episode duration must be a number",
+                            description: "The episode duration (in seconds)"
+                        }
+                    )
+                    .int("The episode duration must be an integer")
+                    .positive("The episode duration must be a positive number"),
+
+                path: zod
+                    .string(
+                        {
+                            required_error: "The episode path is required",
+                            invalid_type_error: "The episode path must be a string",
+                            description: "The episode path"
+                        }
+                    )
+                    .min(1, "The episode path must have a minimum of 1 character")
+                    .max(255, "The episode path must have a maximum of 255 characters"),
+
+                position: zod
+                    .number(
+                        {
+                            required_error: "The episode position is required",
+                            invalid_type_error: "The episode position must be a number",
+                            description: "The episode position"
+                        }
+                    )
+                    .int("The episode position must be an integer")
+            }
+        );
+
+        return createEpisodeSchema.safeParse(episodeData);
+    }
+
+    findOne(episodeData: any) {
+        const findOneEpisodeSchema = zod.object(
+            {
+                id: zod
+                    .number(
+                        {
+                            required_error: "The episode ID is required",
+                            invalid_type_error: "The episode ID must be a number",
+                            description: "The episode ID"
+                        }
+                    )
+                    .int("The episode ID must be an integer")
+                    .positive("The episode ID must be a positive number")
+            }
+        );
+
+        return findOneEpisodeSchema.safeParse(episodeData);
+    }
+
+    findAll(
+        episodeData: {
+            page: number;
+            quantity: number;
+            orderColumn: string;
+            orderBy: string;
+            search: string;
+        }
+    ) {
+        const availableOrders = [
+            "asc",
+            "desc"
+        ];
+
+        const columnsToOrderBy = [
+            "id",
+            "name",
+            "position",
+            "updatedAt"
+        ];
+
+        const { take, skip } = getPaginationParameters(episodeData.page, episodeData.quantity);
+
+        const orderColumn = columnsToOrderBy.includes(episodeData.orderColumn)
+            ? episodeData.orderColumn
+            : "position";
+
+        const orderBy = availableOrders.includes(episodeData.orderBy)
+            ? episodeData.orderBy
+            : "asc";
+
+        const { search } = episodeData;
+
+        return {
+            take,
+            skip,
+            orderColumn,
+            orderBy,
+            search
+        };
+    }
+
+    update(episodeData: any) {
+        const updateEpisodeSchema = zod.object(
+            {
+                id: zod
+                    .number(
+                        {
+                            required_error: "The episode ID is required",
+                            invalid_type_error: "The episode ID must be a number",
+                            description: "The episode ID"
+                        }
+                    )
+                    .int("The episode ID must be an integer")
+                    .positive("The episode ID must be a positive number"),
+
+                name: zod
+                    .string(
+                        {
+                            required_error: "The episode name is required",
+                            invalid_type_error: "The episode name must be a string",
+                            description: "The episode name"
+                        }
+                    )
+                    .min(1, "The episode name must have a minimum of 1 character")
+                    .max(255, "The episode name must have a maximum of 255 characters"),
+
+                seasonId: zod
+                    .number(
+                        {
+                            required_error: "The episode season ID is required",
+                            invalid_type_error: "The episode season ID must be a number",
+                            description: "The episode season ID"
+                        }
+                    )
+                    .int("The episode season ID must be an integer")
+                    .positive("The episode season ID must be a positive number"),
+
+                duration: zod
+                    .number(
+                        {
+                            required_error: "The episode duration is required",
+                            invalid_type_error: "The episode duration must be a number",
+                            description: "The episode duration (in seconds)"
+                        }
+                    )
+                    .int("The episode duration must be an integer")
+                    .positive("The episode duration must be a positive number"),
+
+                path: zod
+                    .string(
+                        {
+                            required_error: "The episode path is required",
+                            invalid_type_error: "The episode path must be a string",
+                            description: "The episode path"
+                        }
+                    )
+                    .min(1, "The episode path must have a minimum of 1 character")
+                    .max(255, "The episode path must have a maximum of 255 characters"),
+
+                position: zod
+                    .number(
+                        {
+                            required_error: "The episode position is required",
+                            invalid_type_error: "The episode position must be a number",
+                            description: "The episode position"
+                        }
+                    )
+                    .int("The episode position must be an integer")
+            }
+        );
+
+        return updateEpisodeSchema.safeParse(episodeData);
+    }
+
+    inactivate(episodeData: any) {
+        const inactivateEpisodeSchema = zod.object(
+            {
+                id: zod
+                    .number(
+                        {
+                            required_error: "The episode ID is required",
+                            invalid_type_error: "The episode ID must be a number",
+                            description: "The episode ID"
+                        }
+                    )
+                    .int("The episode ID must be an integer")
+                    .positive("The episode ID must be a positive number")
+            }
+        );
+
+        return inactivateEpisodeSchema.safeParse(episodeData);
+    }
+}
--- src/validations/index.ts
+++ src/validations/index.ts
@@ -2,3 +2,4 @@ export * from "./UserValidations";
 export * from "./SeriesValidations";
 export * from "./TagValidations";
 export * from "./SeasonValidations";
+export * from "./EpisodeValidations";
