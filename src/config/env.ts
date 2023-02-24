6d689b2d4179741eb6ac6e38ad96e65616afe2fb
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-02-24 06:19:33 -0300

    chore: export series base url environment variable

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,3 +4,4 @@ config();
 
 export const PORT = Number(process.env.PORT) || 3000;
 export const SECRET = String(process.env.SECRET);
+export const SERIES_BASE_URL = String(process.env.SERIES_BASE_URL);
