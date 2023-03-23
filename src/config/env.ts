e111dd5db18230f33d2d039ef4029a2ff24aff59
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-03-23 12:11:59 -0300

    chore: add series folder ignore items environment variable

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,3 +5,4 @@ config();
 export const PORT = Number(process.env.PORT) || 3000;
 export const SECRET = String(process.env.SECRET);
 export const SERIES_BASE_URL = String(process.env.SERIES_BASE_URL);
+export const SERIES_FOLDER_IGNORE_ITEMS = JSON.parse(String(process.env.SERIES_FOLDER_IGNORE_ITEMS)) as Array<string>;
